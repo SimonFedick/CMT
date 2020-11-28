SimonFedick/CMT

commit 4964fd93c7604d940e23140314480826669a1eef

Added console commands and fixed a bug regarding admin deletion.
Console commands "help, deladmin, listadmin, passwd" were added.
A bug has been fixed where upon deletion admins where not removed from an internal temporary list of administrators.

backend/src/main/java/main/ConfigParser.java
@@ -5,10 +5,7 @@ import request.Request;
 import user.Admin;
 import voting.Voting;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConfigParser {
@@ -56,10 +53,27 @@ public class ConfigParser {
                 );
 
         List<Admin> allAdmins = conf.getAllAdmins();
-        conf.deleteAllAdmins();
+        /*
+        Emails are used as unique identifiers for admins.
+        On startup, non existing admins are created from the config file.
+        No passwords from already existing accounts will be overridden, no
+        admins removed from the config file will be automatically removed.
+         */
+        List<String> eMails = new LinkedList<>();
+        allAdmins.forEach(a -> eMails.add(a.getEmail()));
+        //conf.deleteAllAdmins();
+
+        System.out.println("===[Existing Accounts]===");
+        allAdmins.forEach(a -> {
+            System.out.println(a.toString());
+            System.out.println("password: " + conf.getUserPassword(a.getID()).second() + "\n");
+        });
+
+        System.out.println("===[Added Accounts]===");
 
         admins.forEach(a -> {
             Admin admin = new Admin(a.get(0), a.get(1), conf.getFreeUserName(a.get(0)), a.get(2), a.get(3), a.get(4));
+            if(eMails.contains(admin.getEmail())) return;
             conf.addAdmin(admin);
             System.out.println(admin.toString());
             System.out.println("password: " + conf.getUserPassword(admin.getID()).second() + "\n");

backend/src/main/java/main/Conference.java
@@ -429,6 +429,8 @@ public class Conference implements UserManagement, VotingManagement, RequestMana
             adminLock.lock();
             if(admins.get(ID) == null) {
                 throw new IllegalArgumentException("Admin not found");
+            } else {
+                admins.remove(ID);
             }
             if(!db_userManagement.removeUser(ID)) {
                 throw new IllegalArgumentException("Admin can not be removed for unknown reasons");

backend/src/main/java/main/Main.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Main {
 
@@ -25,13 +26,13 @@ public class Main {
      * @param args
      */
     private static void parseArguments(String[] args) {
-        if(args.length != 2) {
+        if (args.length != 2) {
             printUsage();
         } else {
-            if(!args[0].toLowerCase().equals("test") && !args[0].toLowerCase().equals("start")) {
+            if (!args[0].toLowerCase().equals("test") && !args[0].toLowerCase().equals("start")) {
                 printUsage();
-            } else if(args[0].toLowerCase().equals("test")) {
-                switch(args[1]) {
+            } else if (args[0].toLowerCase().equals("test")) {
+                switch (args[1]) {
                     case "normal":
                         startNormalConference(true);
                         break;
@@ -48,13 +49,9 @@ public class Main {
                     fis.close();
                     String str = new String(data, StandardCharsets.UTF_8);
                     conf = ConfigParser.parseConfigFile(str);
-                    CommunicationManager w = new CommunicationManagerFactory(conf).create();
-                    w.start();
-                    char c = (char) System.in.read();
-                    while(c != 'q') {
-                        c = (char) System.in.read();
-                    }
-                    w.stop();
+                    CommunicationManager communicationManager = new CommunicationManagerFactory(conf).create();
+                    communicationManager.start();
+                    listenForCommands(communicationManager);
                 } catch (IOException e) {
                     e.printStackTrace();
                     System.exit(1);
@@ -75,7 +72,7 @@ public class Main {
      **/
     private static void startNormalConference(boolean clean) {
         conf = new Conference(clean);
-        if(conf.getAllAdmins().isEmpty()) {
+        if (conf.getAllAdmins().isEmpty()) {
             conf.addAdmin(new Admin("Admin Administrator", "admin@administrator", "admin", "NoGroup", "AdminAllee", "onlyAdmin"), "admin");
             conf.addAttendee(new Attendee("Test User", "user@test.de", "user", "Tester", "Testerhood", "TestUser"), "user");
         }
@@ -84,18 +81,74 @@ public class Main {
         conf.generateAllQRCodes();
         conf.getAllQrCodes();
 
-        CommunicationManager w = new CommunicationManagerFactory(conf).enableDebugging().create();
-        w.start();
+        CommunicationManager communicationManager = new CommunicationManagerFactory(conf).enableDebugging().create();
+        communicationManager.start();
+        listenForCommands(communicationManager);
+    }
+
+    private static void listenForCommands(CommunicationManager communicationManager) {
+        System.out.println("Type \"help\" for help.");
         System.out.println("Press 'q' to close the server");
-        try {
-            char c = (char) System.in.read();
-            while(c != 'q') {
-                c = (char) System.in.read();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            String[] args = input.split(" ");
+            switch (args[0]) {
+                case "q":
+                    communicationManager.stop();
+                    System.exit(0);
+                    break;
+                case "deladmin":
+                    if(args.length == 2) {
+                        Admin admin = null;
+                        for(Admin admins : conf.getAllAdmins()) {
+                            if(admins.getUserName().equals(args[1])) {
+                                admin = admins;
+                                break;
+                            }
+                        }
+                        if(admin != null) {
+                            conf.removeAdmin(admin.getID());
+                            System.out.println("Administrator " + admin.getUserName() + " has been removed.");
+                        } else {
+                            System.out.println("There is no administrator with username \"" + args[1] + "\"");
+                        }
+                    } else {
+                        System.out.println("Type \"help\" for help.");
+                    }
+                    break;
+                case "listadmins":
+                    conf.getAllAdmins().forEach(a -> {
+                        System.out.println("");
+                        System.out.println(a.toString());
+                        System.out.println("password: " + conf.getUserPassword(a.getID()).second());
+                    });
+                    break;
+                case "passwd":
+                    if(args.length >= 2) {
+                        Admin admin = null;
+                        for(Admin admins : conf.getAllAdmins()) {
+                            if(admins.getUserName().equals(args[1])) {
+                                admin = admins;
+                                break;
+                            }
+                        }
+                        if(admin != null) {
+                            conf.generateNewUserPassword(admin.getID());
+                            System.out.println("New password: " + conf.getUserPassword(admin.getID()).second());
+                        } else {
+                            System.out.println("There is no administrator with username \"" + args[1] + "\"");
+                        }
+                    } else {
+                        System.out.println("Type \"help\" for help.");
+                    }
+                    break;
+                case "help":
+                    System.out.println("q - stops the application");
+                    System.out.println("deladmin <username> - deletes admin with given username (does not delete admins from config)");
+                    System.out.println("listadmins - lists the data of all admins");
+                    System.out.println("passwd <username> - regenerates the password for the given username of an admin");
             }
-            w.stop();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
         }
     }
 

commit d438bdae5677f49ff89907e0aa3b498f4eec579f

Bugfix: Admins removed from config not deleted Removed a bug introduced in 4964fd93c7604d940e23140314480826669a1eef where admins who were removed from the config file were no longer deleted on conference startup.

backend/src/main/java/main/ConfigParser.java
@@ -63,6 +63,17 @@ public class ConfigParser {
         allAdmins.forEach(a -> eMails.add(a.getEmail()));
         //conf.deleteAllAdmins();
 
+        System.out.println("===[Removed Accounts]===");
+        new LinkedList<>(allAdmins).forEach(a -> {
+            for(List<String> admin : admins) {
+                String mail = admin.get(1);
+                if(a.getEmail().equals(mail)) return;
+            }
+            System.out.println(a.toString() + "\n");
+            allAdmins.remove(a);
+            conf.removeAdmin(a.getID());
+        });
+
         System.out.println("===[Existing Accounts]===");
         allAdmins.forEach(a -> {
             System.out.println(a.toString());

backend/src/test/java/config/ConfigParserTests.java
@@ -222,8 +222,8 @@ public class ConfigParserTests {
                 "endTime : '1607731200' # A unix timestamp. This field is mandatory\n" +
                 "url : 'http://localhost' # the url at which the conference is hosted \n" +
                 "admin : 'name1:email1:group:residence:function' \n" +
-                "admin : 'name4:email2:group:residence:function' \n" +
-                "admin : 'name5:email3:group:residence:function' ";
+                "admin : 'name4:email4:group:residence:function' \n" +
+                "admin : 'name5:email5:group:residence:function' ";
 
         c = ConfigParser.parseConfigFile(config);
 
@@ -238,13 +238,13 @@ public class ConfigParserTests {
         AtomicBoolean found3 = new AtomicBoolean(false);
 
         c.getAllAdmins().forEach(a -> {
-            if(a.getName().equals("name1")) {
+            if(a.getEmail().equals("email1")) {
                 found1.set(true);
             }
-            if(a.getName().equals("name4")) {
+            if(a.getEmail().equals("email4")) {
                 found2.set(true);
             }
-            if(a.getName().equals("name5")) {
+            if(a.getEmail().equals("email5")) {
                 found3.set(true);
             }
 